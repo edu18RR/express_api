d0a812250786733d9961bc31f8dc429fbf029be9
edu18RR <edu.rosas.16@gmail.com>
2022-06-18 02:05:48 -0500

    agregando PUT y DELETE

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ app.use(express.json())
 // Puerto en que abriremos nuestra app: localhost:3000
 const port = 3000
 
+//esto es un endpoint
 //routes
 // METHODS HTTP: GET, POST, PUT, DELETE
 app.get('/v1/explorers', (req, res) => {
@@ -32,13 +33,26 @@ app.get('/v1/explorers/:id', (req, res) => { //son para regresar informacion
 })
 
 app.post('/v1/explorers', (req, res) => { //sirve para crear un nuevo registro
-    console.log(`POST Explorers V1 API ${new Date()}`)
-    //agregar logica para persistir en la base de datos
-    console.log(req.body)   //parametros del request
-    res.status(200).json({ message: "Creado exitosamente" })
+    console.log(`PUT Explorers V1 API ${new Date()}`)
+    console.log(req.body)   //parametr para actualizar un registro
+    res.status(201).json({ message: "Creado exitosamente" })
 })
 
+app.put('/v1/explorers/:id', (req, res) => {
+    console.log(`POST Explorers V1 API ${new Date()}`)
+    console.log(req.body)   //parametros del request
+    console.log(req.params.id) //parametro de la url
+    //agregar la logica de actualizar un registro
+    res.status(200).json({ message: "actualizado exitosamente" })
+})
 
+app.delete('/v1/explorers/:id', (req, res) => {
+    console.log(`DELETE Explorers V1 API ${new Date()}`)
+    //Agregar la logica de eliminar un registro envio de un mensaje
+    res.status(200).json({ message: "Se elimino exitosamente" })
+})
+
+//Con esto se inicia la app
 app.listen(port, () => {
     console.log(`Example app listening on port: ${port}`)
 })
